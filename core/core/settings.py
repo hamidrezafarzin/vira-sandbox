61ec45bd66da150e98fe6ffb03698da531c07020
Hamidreza Farzin <hamidfarzin1382@gmail.com>
2023-12-31 18:37:43 +0330

    satatic

--- core/core/settings.py
+++ core/core/settings.py
@@ -120,13 +120,13 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "static"
 STATIC_URL = "/static/"
-
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
+    (BASE_DIR / "staticfiles"),
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
