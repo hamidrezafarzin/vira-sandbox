40e3cdaf39c876b2dad15435ce0c6cafe60d822a
Hamidreza Farzin <hamidfarzin1382@gmail.com>
2023-12-31 17:47:53 +0330

    edit static

--- core/core/settings.py
+++ core/core/settings.py
@@ -118,15 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
+
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
-    (BASE_DIR / "staticfiles"),
+    BASE_DIR / "static",
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
