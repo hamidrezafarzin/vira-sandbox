25762e80b0e69f877845056f75d88e916fc7ae84
Hamidreza Farzin <hamidfarzin1382@gmail.com>
2023-12-31 18:43:59 +0330

    static

--- core/core/settings.py
+++ core/core/settings.py
@@ -123,10 +123,6 @@ STATIC_URL = "/static/"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
-STATICFILES_DIRS = [
-    (BASE_DIR / "staticfiles"),
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
