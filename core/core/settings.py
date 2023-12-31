b2662505f31830958ef168dec15cc3ea4e3c56dd
Hamidreza Farzin <hamidfarzin1382@gmail.com>
2023-12-31 18:48:06 +0330

    change static

--- core/core/settings.py
+++ core/core/settings.py
@@ -119,9 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 STATIC_URL = "/static/"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/scan")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
