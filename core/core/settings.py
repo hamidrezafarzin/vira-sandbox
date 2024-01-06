c13c326af866d6f376d160a462ba89cbe784d1d1
Hamidreza Farzin <hamidfarzin1382@gmail.com>
2024-01-06 16:31:20 +0330

    add import export setting

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scan.apps.ScanConfig'
+    'scan.apps.ScanConfig',
+    'import_export',
 ]
 
 MIDDLEWARE = [
